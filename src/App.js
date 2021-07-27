0e9fb23bbdfe07c4de36f5f4d8f3d0bd7cadf288
slqpez <santiago.lopez16@udea.edu.co>
2021-07-27 16:04:39 -0500

    Pre deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import { Link, Route, Switch } from "react-router-dom";
+import Profile from "./Profile";
+import axios from  'axios';
+import "./App.css";
+
+
+const Home = ()=> <h1>Welcome to home</h1>
+
+const Header =({authenticated, setValue})=>{
+
+  function handleSignInClick  () {
+ 
+    window.open("https://backend-kesesa.herokuapp.com/auth/google", "_self");
+  };
+
+   function handleLogoutClick  () {
+   
+    window.open("https://backend-kesesa.herokuapp.com/auth/logout", "_self");
+    setValue(false)
+    //TODO hacer authenticated false.
+  };
+
+  return(
+    <div>
+      {authenticated ? (
+          <li onClick={handleLogoutClick} style={{cursor: 'pointer', color:"green"}}>Logout</li>
+        ) : (
+          <li onClick={handleSignInClick}>Login</li>
+        )}
+    </div>
+  )
+}
+//http://localhost:5000
+//https://backend-kesesa.vercel.app
+//https://backend-kesesa.herokuapp.com
+
 
 function App() {
+const [value, setValue] = useState({
+  user:{},
+  error:null,
+  authenticated:false,
+})
+
+
+
+console.log(value.authenticated)
+useEffect(() => {
+  axios.get("https://backend-kesesa.herokuapp.com/auth/login/success", {
+    credentials: "include",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Credentials": true,
+    }
+  })
+    .then(responseJson => {
+      console.log(responseJson)
+      setValue({
+        authenticated: true,
+        user: responseJson.user
+      });
+    })
+    .catch(error => {
+      setValue({
+        authenticated: false,
+        error: "Failed to authenticate user"
+      });
+    });
+}, [])
+
+
+ 
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/profile" component={App}>
+        {" "}
+        <Profile />
+      </Route>
+      <Route path="/">
+        {" "}
+        <div className="App">
+         <Header authenticated={value.authenticated} setValue={setValue}></Header>
+         {value.authenticated?<Home/>: "No estás logged."}
+        </div>
+      </Route>
+    </Switch>
   );
 }
 
