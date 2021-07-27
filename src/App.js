d264979fb6a7ead8fcf9e6feb6179824e35616c2
slqpez <santiago.lopez16@udea.edu.co>
2021-07-27 16:09:53 -0500

    Lintern

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import {useState, useEffect} from 'react'
-import { Link, Route, Switch } from "react-router-dom";
+import {  Route, Switch } from "react-router-dom";
 import Profile from "./Profile";
 import axios from  'axios';
 import "./App.css";
